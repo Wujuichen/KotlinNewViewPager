51ff81daaa96d0c689d52f6dc47e061083a24b3d
wujuichen <stu9930603@gmail.com>
2023-06-29 17:29:15 +0800

    change of MyPerson.kt

--- app/src/main/java/com/example/fragmentslide/MyPerson.kt
+++ app/src/main/java/com/example/fragmentslide/MyPerson.kt
@@ -1,8 +1,47 @@
 package com.example.fragmentslide
 
-class MyPerson(
-    val name: String,
-    val gender: Int,
-    val birthday: Long
-) {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+data class User(val name: String, val gender: Int, val birth: Long)
+
+class MyPerson : ListAdapter<User, UserViewHolder>(UserDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.bind(user)
+    }
+}
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+    private val genderTextView: TextView = itemView.findViewById(R.id.ageTextView)
+    private val birthTextView: TextView = itemView.findViewById(R.id.ageTextView)
+
+    fun bind(user: User) {
+        nameTextView.text = user.name
+        genderTextView.text = user.gender.toString()
+        birthTextView.text = user.birth.toString()
+    }
+}
+
+class UserDiffCallback : DiffUtil.ItemCallback<User>() {
+
+    override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+        return oldItem.name == newItem.name
+   }
+
+    override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+        return oldItem == newItem
+    }
 }
