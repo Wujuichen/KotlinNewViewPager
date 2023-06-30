2e4f33a7fd0a06d435859d3d1a12ed36d6463c08
wujuichen <stu9930603@gmail.com>
2023-06-30 17:32:24 +0800

    most are right but some still can not printout words to each fragment

--- app/src/main/java/com/example/fragmentslide/MainActivity.kt
+++ app/src/main/java/com/example/fragmentslide/MainActivity.kt
@@ -2,18 +2,56 @@ package com.example.fragmentslide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.ListAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager2: ViewPager2
-    private lateinit var wordList : ArrayList<Int>
-    //private lateinit var adapter: ListAdapter
+    private lateinit var userList: ArrayList<String>
+    private lateinit var adapter: MyPersonAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager2 = findViewById(R.id.viewPager2)
+
+        val person1 = User("Anne",  1, 12345678)
+        val person2 = User("Jason", 0, 20001125)
+        val person3 = User("Joyce", 1, 20010816)
+
+        userList = ArrayList()
+        userList.add(person1.toString())
+        userList.add(person2.toString())
+        userList.add(person3.toString())
+
+        adapter = MyPersonAdapter(userList,viewPager2)
+        viewPager2.adapter = adapter
+
+    }
+}
+
+        //val myPersonList = mutableListOf(person1, person2, person3)
+
+        //val listadapter = MyPerson(myPersonList)
+        //listadapter.submitList(null)
+
+        //val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        //recyclerView.adapter = listadapter
+
+        //listadapter.submitList(myPersonList)
+
+
+    //private fun generateFragments(userList: List<User>): MutableList<Fragment> {
+    //    val fragments = mutableListOf<Fragment>()
+
+    //    for (user in userList) {
+    //        val fragment = MyPerson.newInstance(user)
+    //        fragments.add(fragment)
+    //    }
+
+    //    return fragments
+    //}
 
         // create fake person
         //val person1 = MyPerson("Anne",  1, 12345678)
@@ -23,5 +61,11 @@ class MainActivity : AppCompatActivity() {
         //val personDetails = listOf(person1,person2,person3)
 
         // ... then create a list of people to insert into your adapter
-    }
-}
+
+//val userList = mutableListOf(
+//   User("Anne", 1, 12345678),
+//   User("Jason", 0, 20001125),
+//   User("Joyce", 1, 20010816)
+//)
+
+
--- app/src/main/java/com/example/fragmentslide/MyPersonAdapter.kt
+++ app/src/main/java/com/example/fragmentslide/MyPersonAdapter.kt
@@ -10,10 +10,10 @@ import androidx.recyclerview.widget.RecyclerView
 
 data class User(val name: String, val gender: Int, val birth: Long)
 
-class MyPerson : ListAdapter<User, UserViewHolder>(UserDiffCallback()) {
+class MyPersonAdapter(user: ArrayList<String>) : ListAdapter<User, UserViewHolder>(UserDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
         return UserViewHolder(view)
     }
 
@@ -22,11 +22,10 @@ class MyPerson : ListAdapter<User, UserViewHolder>(UserDiffCallback()) {
         holder.bind(user)
     }
 }
-
 class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
-    private val genderTextView: TextView = itemView.findViewById(R.id.ageTextView)
-    private val birthTextView: TextView = itemView.findViewById(R.id.ageTextView)
+    private val genderTextView: TextView = itemView.findViewById(R.id.genderTextView)
+    private val birthTextView: TextView = itemView.findViewById(R.id.birthTextView)
 
     fun bind(user: User) {
         nameTextView.text = user.name
@@ -39,7 +38,9 @@ class UserDiffCallback : DiffUtil.ItemCallback<User>() {
 
     override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
         return oldItem.name == newItem.name
-   }
+    //&& oldItem.gender == newItem.gender && oldItem.birth == newItem.birth
+    }
+    // areItemsThsSame 用來判斷兩個items 是否一樣。
 
     override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
         return oldItem == newItem
